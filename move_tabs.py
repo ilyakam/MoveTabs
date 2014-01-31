88940572d561d8bde2bf6568224c34a98a31e20b
Ilya Kaminsky <ilyakam@gmail.com>
2014-01-31 10:34:26 -0800

    Refactored `MoveTabsToDirection` logic to be concise and readable

--- move_tabs.py
+++ move_tabs.py
@@ -21,31 +21,34 @@ class MoveTabsToDirectionCommand(sublime_plugin.WindowCommand):
 
     # Initialize:
     new_group = active_group
-    new_index = None
-
-    # Left:
-    if direction < 0:
-      # Move to group on the left, if possible:
-      if active_index+direction < 0:
-        if active_group-1 >= 0:
-          new_group = active_group-1
-          new_index = len(self.window.views_in_group(new_group))
-        else:
-          new_index = 0
-
-    # Right:
-    else:
-      # Move to group on right, if possible:
-      if active_index+direction >= len(self.window.views_in_group(active_group)):
-        if active_group+1 <= self.window.num_groups()-1:
-          new_group = active_group+1
-          new_index = 0
-        else:
-          new_index = len(self.window.views_in_group(new_group))-1
-
-    # Perform regular move if all exceptions have been caught:
-    if new_index is None:
-      new_index = active_index+direction
-
-    # Move active view to new group and new index:
-    self.window.set_view_index(active_view, new_group, new_index)
+    new_index = active_index
+    
+    # Set boundaries:
+    min_group = 0
+    max_group = self.window.num_groups()-1
+    min_index = 0
+    max_index = len(self.window.views_in_group(active_group))-1
+    
+    # logic:
+    new_index += direction
+    if new_index < min_index:
+      # Move to previous group:
+      new_group -= 1
+      
+      # Recalculate max index:
+      if new_group < min_group:
+        return
+        
+      max_index = len(self.window.views_in_group(new_group))
+      new_index = max_index
+      
+    elif new_index > max_index:
+      # Move to next group:
+      new_group += 1
+      new_index = min_index
+      
+    # Validation:
+    if (min_group <= new_group <= max_group and
+        min_index <= new_index <= max_index
+    ):
+      self.window.set_view_index(active_view, new_group, new_index)
